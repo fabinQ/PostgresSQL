c7ab3cb36a34b926998d4ccbde1a7044c0ef6fa2
fabinQ <maciejfabinski@gmail.com>
2023-07-08 00:18:28 +0200

    Modify union function

--- Section 6/db_northwind_union.sql
+++ Section 6/db_northwind_union.sql
@@ -1,3 +1,21 @@
 select
-*
-from employees
+	e.last_name, e.first_name, 'EMPLOYEES' AS TYPE
+from employees e
+UNION ALL
+select
+	c.contact_name, 
+	substring(c.contact_name, position(' ' in c.contact_name)+1, length(c.contact_name)-position(' ' in c.contact_name)),
+	'CUSTOMER'
+from customers c
+UNION ALL
+select
+	s.contact_name, 
+	substring(s.contact_name, position(' ' in s.contact_name)+1, length(s.contact_name)-position(' ' in s.contact_name)),
+	'SUPPLIER'
+from suppliers s
+UNION ALL
+select
+	s.company_name, 
+	NULL,
+	'SHIPPERS'
+from shippers s
