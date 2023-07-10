7da3723772cff9b11ba9dbfc0a150b29d168070f
fabinQ <maciejfabinski@gmail.com>
2023-07-10 20:53:35 +0200

    Delete recently added records

---
+++ Section 7/db_northwind_delete.sql
@@ -0,0 +1,11 @@
+/*
+select
+*
+from products
+where product_id = 78
+*/
+
+--delete from categories where category_id = 9
+--delete from products where product_id = 78
+--delete from orders where order_id = 11078
+--delete from order_details where order_id = 11078
